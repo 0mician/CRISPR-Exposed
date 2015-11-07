50090b7075fd5131aff07f4ca609b9593a159a67
Milt0n <cedric.lood@gmail.com>
2015-11-07 23:37:02 +0100

    update comments

--- Configuration/VagrantBaseVM.sh
+++ Configuration/VagrantBaseVM.sh
@@ -2,18 +2,18 @@
 
 echo 'provisioning.. '
 
-# installing vagrant
+# installing ansible
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get -y install ansible
 
-# configure vagrant
+# configure ansible
 sudo sed -i "s/\#ask_pass = True/ask_pass = True/" /etc/ansible/ansible.cfg
 # okaying root auth by password for ssh
 sudo sed -i "s/without-password/yes/" /etc/ssh/sshd_config
 sudo service ssh restart
 
-# configure vagrant host
+# configure ansible hosts
 sudo cp /vagrant/hosts /etc/ansible/
 
 echo '.. done!'
