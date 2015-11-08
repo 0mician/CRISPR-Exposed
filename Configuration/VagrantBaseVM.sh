a68450c2bcab2a84104927da35206eafd486c12e
Milt0n <cedric.lood@gmail.com>
2015-11-08 11:21:56 +0100

    issue with the sed command fixed

--- Configuration/VagrantBaseVM.sh
+++ Configuration/VagrantBaseVM.sh
@@ -8,7 +8,7 @@ sudo apt-get update
 sudo apt-get -y install ansible
 
 # configure ansible
-sudo sed -i "s/\#ask_pass = True/ask_pass = True/" /etc/ansible/ansible.cfg
+sudo sed -i "s/\#ask_pass      = True/ask_pass = True/" /etc/ansible/ansible.cfg
 # okaying root auth by password for ssh
 sudo sed -i "s/without-password/yes/" /etc/ssh/sshd_config
 sudo service ssh restart
