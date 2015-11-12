e3dd834b2c6f0ddf82983067b649d5a9db14d89f
hamed <hamed2005@gmail.com>
2015-11-12 22:49:09 +0100

    adding the script for generating crt output --handles multifasta input too

---
+++ Pipelines/multi-fasta-crt.py
@@ -0,0 +1,42 @@
+import os
+import re
+from Utils.fastas import *
+
+crt = "../Configuration/Tools/CRT1.2-CLI.jar"
+data = "../Data/"
+fasta_re = re.compile('.*\_genomic.fna')
+
+dir_list = os.listdir(data)
+
+print("Generating CRT output ...\n")
+
+## loop in genomes in Data directory
+for genome_dir in dir_list:
+    genome_dir_list = os.listdir(data+genome_dir)
+    
+    ## loop in genome directory looking for genome fasta file(s)
+    for fasta_file in genome_dir_list:
+        input_file = re.search(fasta_re,fasta_file)
+        
+        if(input_file):
+            
+            ## set the absolute path to genome dir
+            path = data + genome_dir + '/'
+            fastas = Fastas(path + input_file.group())
+            
+            ## loop in (multi)fasta files using Utils.fastas
+            for fasta in fastas:
+                
+                ## creating a temporary fasta file from a fasta in a multifasta
+                fasta_content = ">" + fasta.header + '\n' + fasta.seq
+                fasta_output_path = path + fasta.header.replace(" ", "_")
+                fasta_output = open(fasta_output_path + ".fasta.tmp", 'w')
+                fasta_output.write(fasta_content)
+                fasta_output.close()
+                
+                ## applying CRT > java -cp /path/to/CRT crt [input] [output]
+                os.system('java -cp ' + crt + ' crt ' + fasta_output_path + ".fasta.tmp" + ' ' + fasta_output_path + '.crt.report')
+                ## removing temporary fasta files
+                os.system('rm ' + path + '*.tmp')
+
+print("Done!\n")
