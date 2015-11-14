c28f1bbde86dedc0fec35c7f328fe938bc2d688d
Milt0n <cedric.lood@gmail.com>
2015-11-14 17:10:44 +0100

    working draft of the strain model

--- webapp/crispr_exposed/crispr/admin.py
+++ webapp/crispr_exposed/crispr/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from crispr.models import Strain
+
+class StrainAdmin(admin.ModelAdmin):
+    list_display = ['refseq_id', 'organism_name', 'group', 'sub_group']
+    fieldsets = (
+        ('General information', {
+            'fields' : ('refseq_id','organism_name', 'group', 'sub_group',
+                        'strain', 'clade_id')
+            }),
+        ('Assembly information', {
+            'fields' : ('assembly', 'scaffolds', 'refseq_ftp', 'genbank_ftp', 
+                        'bio_sample', 'bio_project')
+            }),
+        ('Other information', {
+            'fields' : ('size', 'gc_content', 'genes', 'proteins', 'release_date', 
+                        'modification_date', 'replicons')
+        })
+    )
+
+admin.site.register(Strain, StrainAdmin)
--- webapp/crispr_exposed/crispr/models.py
+++ webapp/crispr_exposed/crispr/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Strain(models.Model):
+
+    ASSEMBLY_LEVEL = (
+        ('COMP', 'Complete Genome'),
+        ('CHRO', 'Chromosome'),
+        ('SCAF', 'Scaffolds'),
+        ('CONT', 'Contigs')
+    )
+
+    refseq_id = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    organism_name = models.CharField(max_length=100)
+    strain = models.CharField(max_length=100)
+    clade_id = models.IntegerField() 
+    bio_sample = models.CharField(max_length=13) # SAMNxxxxxxxx
+    bio_project = models.CharField(max_length=13) # PRJNAxxxxxx
+    group = models.CharField(max_length=50) # phyla name
+    sub_group = models.CharField(max_length=50) # class name
+    assembly = models.CharField(max_length=20) # GCA_0000XXXXX.X 
+    size = models.CharField(max_length=10) # 6.18186 (Mb) (should be integer?)
+    gc_content = models.CharField(max_length=10) # 64.7171 (%) (should be float?)
+    replicons = models.CharField(max_length=2000) # replicons should be split (long list)
+    wgs = models.CharField(max_length=10)
+    scaffolds = models.IntegerField()
+    genes = models.IntegerField()
+    proteins = models.IntegerField()
+    release_date = models.DateField()
+    modification_date = models.DateField()
+    level = models.CharField(max_length=4,choices=ASSEMBLY_LEVEL)
+    refseq_ftp = models.URLField()
+    genbank_ftp = models.URLField()
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Strain, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return "refseq: %s\norganism name: %s" % (self.refseq_id, self.organism_name)
