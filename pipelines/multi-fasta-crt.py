ddb95a74c3b5ab0f331cc511d3987b70bae9348b
Milt0n <cedric.lood@gmail.com>
2015-11-19 09:53:02 +0100

    fix for handling files not found when scaling up

--- pipelines/multi-fasta-crt.py
+++ pipelines/multi-fasta-crt.py
@@ -5,8 +5,8 @@ from utils.fastas import *
 
 crt = "../configuration/tools/CRT1.2-CLI.jar"
 data = "../data/"
-fasta_re = re.compile('.*\_genomic.fna')
-
+error_file = open("crt_error.txt", 'w')
+fasta_re = re.compile('.*\_genomic.fna$')
 dir_list = os.listdir(data)
 
 # tracking progress
@@ -22,26 +22,30 @@ for genome_dir in dir_list:
     ## loop in genome directory looking for genome fasta file(s)
     for fasta_file in genome_dir_list:
         input_file = re.search(fasta_re,fasta_file)
-        
-        if(input_file):
+        try:
+            if(input_file):
             
-            ## set the absolute path to genome dir
-            path = data + genome_dir + '/'
-            fastas = Fastas(path + input_file.group())
-            
-            ## loop in (multi)fasta files using Utils.fastas
-            for fasta in fastas:
+                ## set the absolute path to genome dir
+                path = data + genome_dir + '/'
+                fastas = Fastas(path + input_file.group())
                 
-                ## creating a temporary fasta file from a fasta in a multifasta
-                fasta_content = ">" + fasta.header + '\n' + fasta.seq
-                fasta_output_path = path + fasta.header.split(' ', 1)[0]
-                fasta_output = open(fasta_output_path + ".fasta.tmp", 'w')
-                fasta_output.write(fasta_content)
-                fasta_output.close()
+                ## loop in (multi)fasta files using Utils.fastas
+                for fasta in fastas:
                 
-                ## applying CRT > java -cp /path/to/CRT crt [input] [output]
-                os.system('java -cp ' + crt + ' crt ' + fasta_output_path + ".fasta.tmp" + ' ' + fasta_output_path + '.crt.report' + '> /dev/null 2>&1')
-                ## removing temporary fasta files
-                os.system('rm ' + path + '*.tmp')
+                    ## creating a temporary fasta file from a fasta in a multifasta
+                    fasta_content = ">" + fasta.header + '\n' + fasta.seq
+                    fasta_output_path = path + fasta.header.split(' ', 1)[0]
+                    fasta_output = open(fasta_output_path + ".fasta.tmp", 'w')
+                    fasta_output.write(fasta_content)
+                    fasta_output.close()
+                    
+                    ## applying CRT > java -cp /path/to/CRT crt [input] [output]
+                    os.system('java -cp ' + crt + ' crt ' + fasta_output_path + ".fasta.tmp" + ' ' + fasta_output_path + '.crt.report' + '> /dev/null 2>&1')
+                    ## removing temporary fasta files
+                    os.system('rm ' + path + '*.tmp')
+        except FileNotFoundError as e:
+            print("not found")
+            error_file.write(e)
 
+error_file.close()
 print("Done!\n")
