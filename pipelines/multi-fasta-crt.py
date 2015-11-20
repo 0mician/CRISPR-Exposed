e095b6fdb3b2a703846e8a15a17f3d2f46497b44
hamed <hamed2005@gmail.com>
2015-11-20 20:26:17 +0100

    sync with milt0n

--- pipelines/multi-fasta-crt.py
+++ pipelines/multi-fasta-crt.py
@@ -20,25 +20,15 @@ for genome_dir in dir_list:
     count += 1
 
     ## loop in genome directory looking for genome fasta file(s)
-<<<<<<< HEAD
-    for _file in genome_dir_list:
-        fasta_file = re.search(fasta_re, _file)
-        
-        if(fasta_file):
+    for fasta_file in genome_dir_list:
+        input_file = re.search(fasta_re,fasta_file)
+        try:
+            if(input_file):
             
-            ## set the absolute path to genome dir
-            path = data + genome_dir + '/'
-            fastas = Fastas(path + fasta_file.group())
-            
-            ## loop in (multi)fasta files using Utils.fastas
-            for fasta in fastas:
+                ## set the absolute path to genome dir
+                path = data + genome_dir + '/'
+                fastas = Fastas(path + input_file.group())
                 
-                ## creating a temporary fasta file from a fasta in a multifasta
-                fasta_content = ">" + fasta.header + '\n' + fasta.seq
-                fasta_output_path = path + fasta.header.split(' ', 1)[0]
-                fasta_output = open(fasta_output_path + ".fasta.tmp", 'w')
-                fasta_output.write(fasta_content)
-                fasta_output.close()
                 ## loop in (multi)fasta files using Utils.fastas
                 for fasta in fastas:
                     if(len(fasta.seq) < 5000):
