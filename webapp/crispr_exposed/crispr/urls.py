55b1b4da9fd05c8c97852c1be551ce1e0d968d08
hamed <hamed2005@gmail.com>
2015-11-24 22:55:40 +0100

    blast and blast_result urls added

--- webapp/crispr_exposed/crispr/urls.py
+++ webapp/crispr_exposed/crispr/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search_result/$', views.search_result, name='search_result'),
     url(r'^details/(?P<slug>.*?)/$', views.crispr_details, name='crispr_details'),
+    url(r'^blast/$', views.blast, name='blast'),
+    url(r'^blast/result/$', views.blast_result, name='blast_result'),
 ]
