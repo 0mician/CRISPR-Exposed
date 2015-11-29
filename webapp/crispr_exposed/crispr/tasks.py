7642641ca525b1785c495187d5f10821b4d58cf7
hamed <hamed2005@gmail.com>
2015-11-29 22:39:18 +0100

    using temp file names now to avoid conflicts on server

--- webapp/crispr_exposed/crispr/tasks.py
+++ webapp/crispr_exposed/crispr/tasks.py
@@ -1,32 +1,33 @@
 from celery import task
+import tempfile
 
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 @task()
 def blastn(FASTA):
-    fasta_file = open(os.path.join(BASE_DIR, "crispr/blast/tmp/input.fasta") ,'w')
+    ## generatign random file names
+    fasta_temp = tempfile.NamedTemporaryFile()
+    blast_temp = tempfile.NamedTemporaryFile()
+    
+    fasta_file = open(os.path.join(BASE_DIR, "crispr/blast", fasta_temp.name) ,'w')
     fasta_file.writelines(">input\n"+FASTA)
     fasta_file.close()
     
     ## blastn command
     os.system("blastn -query " + 
-              str(os.path.join(BASE_DIR, 'crispr/blast/tmp/input.fasta')) + " -db " + 
+              str(os.path.join(BASE_DIR, 'crispr/blast', fasta_temp.name)) + " -db " + 
               str(os.path.join(BASE_DIR, 'crispr/blast/db/spacers.fasta')) + " -out " + 
-              str(os.path.join(BASE_DIR, 'crispr/blast/tmp/blast_result.txt')))
+              str(os.path.join(BASE_DIR, 'crispr/blast', blast_temp.name)))
 
     ## loop until file is generated
     while(True):
         try:
-            blast_result_file = open(os.path.join(BASE_DIR, "crispr/blast/tmp/blast_result.txt"), 'r')
+            blast_result_file = open(os.path.join(BASE_DIR, "crispr/blast", blast_temp.name), 'r')
             if(blast_result_file):
                 ## reading blast result file into memory
                 blast_result_txt = blast_result_file.read()
                 blast_result_file.close()
-                
-                ## removing temp files
-                os.system("rm " + str(os.path.join(BASE_DIR, "crispr/blast/tmp/input.fasta")) +
-                          " " + str(os.path.join(BASE_DIR, "crispr/blast/tmp/blast_result.txt")))
                 break
         except File.DoesNotExist:
             pass
