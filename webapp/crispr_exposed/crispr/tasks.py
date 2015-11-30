da1891cb37e79aa83b6cb34dd4795a6395f16a2a
hamed <hamed2005@gmail.com>
2015-11-30 13:32:50 +0100

    removed while(true) loop

--- webapp/crispr_exposed/crispr/tasks.py
+++ webapp/crispr_exposed/crispr/tasks.py
@@ -15,21 +15,24 @@ def blastn(FASTA):
     fasta_file.close()
     
     ## blastn command
-    os.system("blastn -query " + 
-              str(os.path.join(BASE_DIR, 'crispr/blast', fasta_temp.name)) + " -db " + 
-              str(os.path.join(BASE_DIR, 'crispr/blast/db/spacers.fasta')) + " -out " + 
-              str(os.path.join(BASE_DIR, 'crispr/blast', blast_temp.name)))
+    exit_code = os.system("blastn -query " +
+                str(os.path.join(BASE_DIR, 'crispr/blast', fasta_temp.name)) + " -db " +
+                str(os.path.join(BASE_DIR, 'crispr/blast/db/spacers.fasta')) + " -out " +
+                str(os.path.join(BASE_DIR, 'crispr/blast', blast_temp.name)))
 
     ## loop until file is generated
-    while(True):
+    blast_result_txt = ''
+    if(exit_code == 0):
         try:
             blast_result_file = open(os.path.join(BASE_DIR, "crispr/blast", blast_temp.name), 'r')
             if(blast_result_file):
                 ## reading blast result file into memory
                 blast_result_txt = blast_result_file.read()
                 blast_result_file.close()
-                break
+                #break
         except File.DoesNotExist:
-            pass
+            pass        ## raise?
+    else:
+        blast_result_txt = "Oops...!\tSorry, something went wrong while running blast!\nPlease try again."
     return blast_result_txt
 
