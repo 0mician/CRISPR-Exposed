a63fe45dc1be8c2c5a2970b75aa639eb9ccd24c1
Milt0n <cedric.lood@gmail.com>
2015-12-10 17:29:32 +0100

    routing home

--- webapp/crispr_exposed/crispr_exposed/urls.py
+++ webapp/crispr_exposed/crispr_exposed/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 import crispr_exposed.apiurls
 
 urlpatterns = [
+    url(r'^$', include('crispr.urls')),
     url(r'^rest-api/', include('crispr_exposed.apiurls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^crispr/', include('crispr.urls')),
